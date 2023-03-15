63141ddf1431799070d6b711825232ccd6119ce2
Impasse <i@uuz.io>
2023-03-15 14:55:21 +0800

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# NoSensor
+
+使 app 在启动旋转锁定时不根据重力感应旋转
+
+---
+
+使用 [YukiHookAPI](https://github.com/fankes/YukiHookAPI)
---
+++ app/src/main/kotlin/io/uuz/xposed/nosensor/HookEntry.kt
@@ -0,0 +1,21 @@
+package io.uuz.xposed.nosensor
+
+import com.highcapable.yukihookapi.YukiHookAPI.Configs.debugLog
+import com.highcapable.yukihookapi.YukiHookAPI.Configs.isDebug
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+
+@InjectYukiHookWithXposed
+class HookEntry : IYukiHookXposedInit {
+    override fun onInit() {
+        debugLog {
+            tag = BuildConfig.APPLICATION_ID
+        }
+        isDebug = BuildConfig.DEBUG
+    }
+
+    override fun onHook() = encase {
+        loadHooker(ActivityHooker)
+    }
+}
